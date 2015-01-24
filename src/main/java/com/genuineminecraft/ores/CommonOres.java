a1457213094c1f3afae5a9c8112ca28baf87fc54
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2015-01-23 19:04:13 -0800

    Version and Image Fixes

--- src/main/java/com/genuineminecraft/ores/CommonOres.java
+++ src/main/java/com/genuineminecraft/ores/CommonOres.java
@@ -26,7 +26,7 @@ public class CommonOres {
 	public static CommonOres instance;
 	public static final String MODID = "CommonOres";
 	public static final String NAME = "Common Ores";
-	public static final String VERSION = "1.7.10-r3";
+	public static final String VERSION = "1.7.10-1.0.5";
 	public static Config config;
 
 	@EventHandler

190e8a10a0126b627bc400fb86ccd4c908f0db13
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2015-02-08 18:08:25 -0800

    Event added.

---
+++ src/main/java/com/genuineflix/metal/event/OreRegistrationEvent.java
@@ -0,0 +1,27 @@
+package com.genuineflix.metal.event;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.oredict.OreDictionary.OreRegisterEvent;
+
+import com.genuineflix.metal.registry.Metal;
+import com.genuineflix.metal.registry.MetalRegistry;
+import com.genuineflix.metal.util.StringHelper;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+public class OreRegistrationEvent {
+
+	@SubscribeEvent
+	public void registerOreEvent(final OreRegisterEvent event) {
+		if (MetalRegistry.isRegistryClosed() || !MetalRegistry.isCommonName(event.Name) || !(event.Ore.getItem() instanceof ItemBlock))
+			return;
+		final Block block = ((ItemBlock) event.Ore.getItem()).field_150939_a;
+		final int meta = event.Ore.getItemDamage();
+		MetalRegistry.cacheCommonBlock(event.Name, block, meta);
+		final Metal metal = MetalRegistry.getMetal(StringHelper.cleanName(event.Name));
+		if (metal == null)
+			return;
+		metal.setGeneration(true);
+	}
+}
